6d24e0ed129f88788bb520574d98ea57cebab967
Unix13 <vivasjesus80@gmail.com>
2022-01-17 19:35:42 +0100

    Terminada clase Paciente

--- src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Paciente.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Paciente.java
@@ -158,4 +158,32 @@ public class Paciente {
 		
 		return iniciales;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paciente other = (Paciente) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return ("nombre=" + nombre + " (" + getIniciales().toUpperCase() + ")" + ", DNI=" + dni + ", teléfono=" + telefono);
+	}
 }
