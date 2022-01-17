83e5788220b856d1a6ea611a44d0a0c899a56115
Unix13 <vivasjesus80@gmail.com>
2022-01-17 19:01:21 +0100

    Creo getters y setters

--- src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Paciente.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Paciente.java
@@ -11,5 +11,57 @@ public class Paciente {
 	private String dni;
 	private String telefono;
 	
+	
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		if(nombre == null || nombre.trim().isEmpty()) {
+			throw new NullPointerException("ERROR: El nombre de un paciente no puede ser nulo o vacío.");
+		}
+		
+		nombre=formateaNombre(nombre);
+		this.nombre = nombre;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	private void setDni(String dni) {
+		if(dni == null || dni.trim().isEmpty()) {
+			throw new NullPointerException("ERROR: El DNI de un paciente no puede ser nulo o vacío.");
+		}
+		dni=dni.toUpperCase();
+		if(comprobarLetraDni(dni)) {
+			this.dni = dni;
+		}else {
+			throw new IllegalArgumentException("ERROR: El DNI no tiene un formato válido.");
+		}
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(String telefono)  {
+		if(telefono == null || telefono.trim().isEmpty()) {
+			throw new NullPointerException("ERROR: El teléfono de un paciente no puede ser nulo o vacío.");
+		}
+		Pattern patron;
+        Matcher comparador;
+        patron = Pattern.compile(ER_TELEFONO);
+        comparador = patron.matcher(telefono);
+        if (comparador.matches()) {
+        	this.telefono = telefono;
+        }else {
+        	throw new IllegalArgumentException("ERROR: El teléfono no tiene un formato válido.");
+        }
+	}
 
 }
