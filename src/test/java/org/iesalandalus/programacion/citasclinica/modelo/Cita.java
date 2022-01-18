c167ecce0f928f707c09b533209362a0aba43601
Unix13 <vivasjesus80@gmail.com>
2022-01-18 18:26:46 +0100

    Creo atributos clase Cita

---
+++ src/test/java/org/iesalandalus/programacion/citasclinica/modelo/Cita.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.citasclinica.modelo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Cita {
+	
+	public static final String FORMATO_FECHA_HORA = "dd/MM/yyyy HH:mm";
+	private LocalDateTime fechaHora;
+	private Paciente paciente;
+
+	
+}
