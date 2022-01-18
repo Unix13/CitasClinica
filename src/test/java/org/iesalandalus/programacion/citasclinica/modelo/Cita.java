51b660326e9d79a5431ec2cc63d42c2d528e5bd9
Unix13 <vivasjesus80@gmail.com>
2022-01-18 18:28:51 +0100

    Creo getters y setters de la clase Cita

--- src/test/java/org/iesalandalus/programacion/citasclinica/modelo/Cita.java
+++ src/test/java/org/iesalandalus/programacion/citasclinica/modelo/Cita.java
@@ -4,10 +4,32 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Cita {
-	
+
 	public static final String FORMATO_FECHA_HORA = "dd/MM/yyyy HH:mm";
 	private LocalDateTime fechaHora;
 	private Paciente paciente;
 
-	
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
+	}
+
+	public void setFechaHora(LocalDateTime fechaHora) {
+		if (fechaHora == null) {
+			throw new NullPointerException("ERROR: La fecha y hora de una cita no puede ser nula.");
+		}
+
+		this.fechaHora = fechaHora;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		if (paciente == null) {
+			throw new NullPointerException("ERROR: El paciente de una cita no puede ser nulo.");
+		}
+		this.paciente = new Paciente(paciente);
+	}
+
 }
