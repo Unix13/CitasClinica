500192dd81730a9f0c03d052367d08966743d214
Unix13 <vivasjesus80@gmail.com>
2022-01-19 20:12:41 +0100

    Creo el método borrar cita

--- src/main/java/org/iesalandalus/programacion/citasclinica/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/MainApp.java
@@ -16,23 +16,16 @@ public class MainApp {
 	private static final int NUM_MAX_CITAS = 30;
 	private static Citas citasClinica;
 
+	private static void insertarCita() {
+		Cita cita;
+		try {
+			cita = new Cita(Consola.leerCita());
+			citasClinica.insertar(cita);
+		} catch (OperationNotSupportedException e) {
 
-		
-		
-		
-		
-		private static void insertarCita() {
-			Cita cita;
-			try {
-				cita=new Cita(Consola.leerCita());
-				citasClinica.insertar(cita);
-			} catch (OperationNotSupportedException e) {
-				
-				e.printStackTrace();
-			}
+			e.printStackTrace();
 		}
-		
-	
+	}
 
 	private static void buscarCita() {
 		LocalDateTime fecha;
@@ -51,5 +44,22 @@ public class MainApp {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
+	private static void borrarCita() {
+		LocalDateTime fecha;
+		Cita citaABorrar, citaEncontrada;
+		try {
+			Paciente pacienteEjemplo = new Paciente("Ejemplo Ejemplo Ejemplo", "11223344b", "950999999");
+			fecha = Consola.leerFechaHora();// podria estar fuera del try
+			citaABorrar = new Cita(pacienteEjemplo, fecha);
+			if (citasClinica.buscar(citaABorrar) == null) {
+				System.out.println("No se ha encontrado la cita");
+			} else {
+				citaEncontrada = new Cita(citasClinica.buscar(citaABorrar));
+				citasClinica.borrar(citaEncontrada);
+			}
+		} catch (IllegalArgumentException | OperationNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
