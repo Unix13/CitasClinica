38b71276bc0871ddad8a8244d37063b02a081a22
Unix13 <vivasjesus80@gmail.com>
2022-01-19 19:19:02 +0100

    Creo método desplazar posición a la izquierda

--- src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Citas.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Citas.java
@@ -6,96 +6,108 @@ import java.time.LocalDateTime;
 import javax.naming.OperationNotSupportedException;
 
 public class Citas {
-	
-	private Cita [] coleccionCitas;
-	private int capacidad; 
+
+	private Cita[] coleccionCitas;
+	private int capacidad;
 	private int tamano;
-	
+
 	public int getCapacidad() {
 		return capacidad;
 	}
-	
+
 	public int getTamano() {
 		return tamano;
 	}
-	
+
 	public Cita[] getCitas() {
 		return coleccionCitas;
 	}
-	
+
 	public Cita[] getCitas(LocalDate fecha) {
-		if (fecha==null) {
+		if (fecha == null) {
 			throw new NullPointerException("ERROR: No se pueden devolver las citas para un día nulo.");
 		}
-		Cita[] coleccionCitasFecha= new Cita[tamano];
-	
-		int i,j=0;
-		for (i=0; i<tamano;i++) {
-			if(coleccionCitas[i].getFechaHora().toLocalDate().equals(fecha)){
-			
-				coleccionCitasFecha[j]= new Cita(coleccionCitas[i]);
+		Cita[] coleccionCitasFecha = new Cita[tamano];
+
+		int i, j = 0;
+		for (i = 0; i < tamano; i++) {
+			if (coleccionCitas[i].getFechaHora().toLocalDate().equals(fecha)) {
+
+				coleccionCitasFecha[j] = new Cita(coleccionCitas[i]);
 				j++;
 			}
 		}
 		return coleccionCitasFecha;
 	}
-	private boolean tamanoSuperado (int nuevoTamano) {
-		boolean superado=false;
-		if (nuevoTamano>=tamano) {
-			superado=true;
+
+	private boolean tamanoSuperado(int nuevoTamano) {
+		boolean superado = false;
+		if (nuevoTamano >= tamano) {
+			superado = true;
 		}
 		return superado;
 	}
-	
-	
+
 	private boolean capacidadSuperada(int nuevoTamano) {
-		boolean superado=false;
-		if (nuevoTamano>=capacidad) {
-			superado=true;
+		boolean superado = false;
+		if (nuevoTamano >= capacidad) {
+			superado = true;
 		}
 		return superado;
 	}
+
 	private int buscarIndice(Cita cita) {
-		int indice=tamano+1; 
+		int indice = tamano + 1;
 		int i;
-		for (i=0; i<tamano;i++) {
-			if (coleccionCitas[i].equals(cita)){ 
-				
-				indice=i;
+		for (i = 0; i < tamano; i++) {
+			if (coleccionCitas[i].equals(cita)) {
+
+				indice = i;
 			}
 		}
-		return indice ;
+		return indice;
 	}
-	public void insertar(Cita cita) throws OperationNotSupportedException{
-		if (cita==null) {
+
+	public void insertar(Cita cita) throws OperationNotSupportedException {
+		if (cita == null) {
 			throw new NullPointerException("ERROR: No se puede insertar una cita nula.");
 		}
-		
-		if(buscar(cita)!=null) {
+
+		if (buscar(cita) != null) {
 			throw new OperationNotSupportedException("ERROR: Ya existe una cita para esa fecha y hora.");
 		}
-		
-		
-		if (capacidadSuperada(tamano)==true) {
+
+		if (capacidadSuperada(tamano) == true) {
 			throw new OperationNotSupportedException("ERROR: No se aceptan más citas.");
-		}else {
-			coleccionCitas[tamano]=new Cita(cita); 
+		} else {
+			coleccionCitas[tamano] = new Cita(cita);
 			System.out.println("Cita introducida correctamente.");
 			tamano++;
-		}		
+		}
 	}
-	public Cita buscar(Cita cita) throws OperationNotSupportedException  {
-		if(cita==null) {
+
+	public Cita buscar(Cita cita) throws OperationNotSupportedException {
+		if (cita == null) {
 			throw new NullPointerException("ERROR: No se puede buscar una cita nula.");
 		}
 		int indice;
-		Cita citaEncontrada = null; //si no encuentra cita este método devuelve null.
-		indice=buscarIndice(cita);
-		if (indice!=tamano+1) {
-			citaEncontrada=new Cita(coleccionCitas[indice]);//obtengo una copia de la cita
+		Cita citaEncontrada = null;
+		indice = buscarIndice(cita);
+		if (indice != tamano + 1) {
+			citaEncontrada = new Cita(coleccionCitas[indice]);
 		}
 
-		return citaEncontrada; //devuelvo una copia de la cita encontrada
+		return citaEncontrada;
 	}
-	
+
+	private void desplazarUnaPosicionHaciaIzquierda(int indice) {
+		int i;
+		for (i = 0; i < tamano; i++) {
+			if (i > indice) {
+				coleccionCitas[i - 1] = coleccionCitas[i];
+			}
+		}
+		coleccionCitas[i] = null;
+	}
+
 }
