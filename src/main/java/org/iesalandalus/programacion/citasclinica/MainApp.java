6909af7d3abaece01b7171b543fb1c564209b409
Unix13 <vivasjesus80@gmail.com>
2022-01-19 20:09:31 +0100

    Creo el método insertar cita en el mainapp

--- src/main/java/org/iesalandalus/programacion/citasclinica/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/MainApp.java
@@ -1,9 +1,37 @@
 package org.iesalandalus.programacion.citasclinica;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.citasclinica.modelo.Cita;
+import org.iesalandalus.programacion.citasclinica.modelo.Citas;
+import org.iesalandalus.programacion.citasclinica.modelo.Paciente;
+import org.iesalandalus.programacion.citasclinica.vista.Consola;
+import org.iesalandalus.programacion.citasclinica.vista.Opciones;
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
+	private static final int NUM_MAX_CITAS = 30;
+	private static Citas citasClinica;
 
 	public static void main(String[] args) {
-		System.out.println("Programa para gestionar las citas de la ClÃ­nica.");
+		System.out.println("Programa para gestionar las citas de la Clínica.");
+		
+		
+		
+		private static void insertarCita() {
+			Cita cita;
+			try {
+				cita=new Cita(Consola.leerCita());
+				citasClinica.insertar(cita);
+			} catch (OperationNotSupportedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 	
-}
+
